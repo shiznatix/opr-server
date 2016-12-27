2d9d520ba7b30bb4769334b43dd8e151ab235227
Andrew Karell <andrew.karell@pipedrive.com>
2016-12-27 21:59:02 +0200

    Nightly build

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const currentDir = __dirname,
 	config = require(`${currentDir}/config/config.json`),
 	logger = require(`${currentDir}/lib/logger.js`),
-	Shows = require(`${currentDir}/lib/shows.js`),
+	shows = require(`${currentDir}/lib/shows.js`),
 	Browse = require(`${currentDir}/lib/browse.js`),
 	db = require(`${currentDir}/lib/db.js`),
+	validator = require(`${currentDir}/lib/validator.js`),
 	express = require('express'),
 	bodyParser = require('body-parser'),
 	_ = require('lodash'),
@@ -11,69 +12,66 @@ const currentDir = __dirname,
 
 function errorResponse(response, error, data = null) {
 	response.status(400).json({
-		'success': false,
-		'data': data,
-		'error': error,
+		success: false,
+		data: data,
+		error: error,
 	});
 }
 function successResponse(response, data = null) {
 	response.status(200).json({
-		'success': true,
-		'data': data,
+		success: true,
+		data: data,
 	});
 }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	'extended': true,
+	extended: true,
 }));
 
 // API routes
-app.post('/random', function(request, response) {
-	shows.random(request.body.showNames, request.body.amount)
-		.then(function(data) {
-			jsonResponse(response, data);
-		})
-		.catch(function(error) {
-			basicFailure(response, error);
-		});
+app.post('/random', (request, response) => {
+	validator.random(request.body).then((params) => {
+		return shows.random(params.showNames, params.amount);
+	}).then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
-app.post('/browse', function(request, response) {
+app.post('/browse', (request, response) => {
 	const browse = new Browse;
 
-	browse.getFiles(request.body.path)
-		.then(function(data) {
-			jsonResponse(response, data);
-		})
-		.catch(function(error) {
-			basicFailure(response, error);
-		});
+	browse.getFiles(request.body.path).then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
-app.get('/shows', function(request, response) {
-	shows.getByCategories()
-		.then(function(data) {
-			successResponse(response, data);
-		})
-		.catch(function(error) {
-			errorResponse(response, error);
-		});
+app.get('/shows', (request, response) => {
+	shows.getByCategories().then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
 
-app.get('*', function(request, response) {
+app.get('*', (request, response) => {
 	response.status(404).json({
-		'success': false,
+		success: false,
 	});
 });
 
 // make sure our DB exists before we start up
 db.initDb().then(() => {
 	// Start web server
-	app.listen(config.port, function() {
+	app.listen(config.port, () => {
 		logger.info('Started server (plain text) on port ' + config.port);
 	});
 
 	// Start scanning directories
-	const shows = new Shows;
 	shows.startScan();
-	shows.getByCategories();
 });
