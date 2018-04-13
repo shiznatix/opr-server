cea06d41e2e018e8a0ffc884293a47856bf7bc38
shiznatix <github@shiznatix.com>
2018-04-13 05:55:53 -0500

    fixed installer and added time to logger

--- install.sh
+++ install.sh
@@ -20,6 +20,15 @@ cd "$(dirname "$0")"
 echo "NPM install..."
 sudo -u pi npm install
 
+printf "Creating config file... "
+if [ ! -f config/config.json ]
+then
+	sudo -u pi cp config/config.example.json config/config.json
+	echo "Default config file created"
+else
+	echo "Config file already existed"
+fi
+
 INIT_SCRIPT_TEMPLAT=`cat opr-server.template`
 INIT_SCRIPT="${INIT_SCRIPT_TEMPLAT/\%INSTALL_PATH\%/$PWD}"
 
@@ -30,5 +39,11 @@ chmod +x /etc/init.d/opr-server
 echo "Creating log file..."
 mkdir -p /var/log/opr
 touch /var/log/opr/server.log
+
+echo "Registering at startup..."
+update-rc.d opr-server defaults
+
+echo "Starting server..."
+service opr-server start
 
 echo "Installation complete!"
